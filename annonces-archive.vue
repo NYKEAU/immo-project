87be17b485ffda85f113e27f15da3e667d558907
MathisPoissel <mathis.poissel@gmail.com>
2024-02-08 17:25:51 +0100

    Création Page Annonces Archive

---
+++ annonces-archive.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Page Annonce Archive</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LegalPage",
+};
+</script>
+
+<style scoped>
+h1 {
+    text-align: center;
+    padding: 20px;
+}
+
+p {
+    text-align: center;
+}
+</style>
