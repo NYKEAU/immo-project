4df19352d8314a7f8b7d1ccd3670239f813f1476
MathisPoissel <mathis.poissel@gmail.com>
2024-02-15 14:23:34 +0100

    Annonce Single Setup

--- annonces-single.vue
+++ annonces-single.vue
@@ -2,11 +2,19 @@
     <div>
         <h1>Page Annonce Single</h1>
     </div>
+    <div>
+        <h1>Titre de l'Annonce</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum veritatis eveniet exercitationem incidunt cum
+            eligendi, illo aperiam ipsam fuga consectetur maiores blanditiis explicabo non quo quia mollitia illum quisquam
+            libero.</p>
+    </div>
+
+    <footer />
 </template>
 
 <script>
 export default {
-    name: "LegalPage",
+    name: "Annonces",
 };
 </script>
 
